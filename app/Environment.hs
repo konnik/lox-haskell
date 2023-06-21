1fab764aa9c0f30a927f16e33959abbf02bce137
konnik <niklas.konstenius@gmail.com>
2023-06-21 16:24:04 +0200

    support for lexical scopes in environment

--- app/Environment.hs
+++ app/Environment.hs
@@ -4,39 +4,59 @@ module Environment (
     declareVar,
     getVar,
     setVar,
+    enterBlock,
+    leaveBlock,
 )
 where
 
 import Data.Map (Map)
 import Data.Map qualified
 
+import Data.Maybe (fromJust)
 import Value (Value)
 
 data Environment = Environment
-    { decls :: Map String Value
+    { variables :: Map String Value
+    , parent :: Maybe Environment
     }
 
 declareVar :: String -> Value -> Environment -> Environment
 declareVar name val env =
     env
-        { decls =
-            Data.Map.insert name val env.decls
+        { variables =
+            Data.Map.insert name val env.variables
         }
 
 getVar :: String -> Environment -> Either String Value
 getVar name env =
-    case Data.Map.lookup name env.decls of
+    case Data.Map.lookup name env.variables of
         Just val -> Right val
-        Nothing -> Left $ "Undefined variable: '" ++ name ++ "'"
+        Nothing -> case env.parent of
+            Just parent ->
+                getVar name parent
+            Nothing ->
+                Left $ "Undefined variable: '" ++ name ++ "'"
 
 setVar :: String -> Value -> Environment -> Either String Environment
 setVar name val env =
-    if Data.Map.member name env.decls
-        then pure $ env{decls = Data.Map.insert name val env.decls}
-        else Left $ "Varable not declared: '" ++ name ++ "'"
+    if Data.Map.member name env.variables
+        then pure $ env{variables = Data.Map.insert name val env.variables}
+        else case env.parent of
+            Just parent -> do
+                parent' <- setVar name val parent
+                pure $ env{parent = Just parent'}
+            Nothing ->
+                Left $ "Varable not declared: '" ++ name ++ "'"
+
+enterBlock :: Environment -> Environment
+enterBlock parent = newEnvironment{parent = Just parent}
+
+leaveBlock :: Environment -> Environment
+leaveBlock env = fromJust env.parent
 
 newEnvironment :: Environment
 newEnvironment =
     Environment
-        { decls = Data.Map.empty
+        { variables = Data.Map.empty
+        , parent = Nothing
         }
